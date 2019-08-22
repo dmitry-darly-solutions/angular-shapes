9895d0b489701919cb3118250bcff93b97950cf3
dmitry-darly-solutions <dmitry@darly.solutions>
2019-08-22 19:27:13 +0300

    Move store module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { InjectionToken, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { reducers } from './core/store';
-
-export const APPReducerToken = new InjectionToken(
-  'APP Registered Reducers',
-);
-
-export function getReducers() {
-  return reducers;
-}
-
-export const ReducerProvider = [
-  { provide: APPReducerToken, useFactory: getReducers },
-];
 
 @NgModule({
   declarations: [
@@ -27,9 +13,8 @@ export const ReducerProvider = [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    StoreModule.forRoot(APPReducerToken, {}),
   ],
-  providers: [ReducerProvider],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,9 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule, InjectionToken } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
+import { reducers } from '../core/store/reducers';
+
+export const APPReducerToken = new InjectionToken(
+  'APP Registered Reducers',
+);
+
+export function getReducers() {
+  return reducers;
+}
+
+export const ReducerProvider = [
+  { provide: APPReducerToken, useFactory: getReducers },
+];
 
 @NgModule({
   declarations: [HomeComponent],
@@ -11,6 +25,8 @@ import { SharedModule } from '../shared/shared.module';
     CommonModule,
     HomeRoutingModule,
     SharedModule,
-  ]
+    StoreModule.forRoot(APPReducerToken, {}),
+  ],
+  providers: [ReducerProvider]
 })
 export class HomeModule { }
