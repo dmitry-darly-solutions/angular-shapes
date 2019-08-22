f05db8001a17f0f7400c903a7c731d4a516b18dd
dmitry-darly-solutions <dmitry@darly.solutions>
2019-08-22 19:05:41 +0300

    Add polyfills

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -17,16 +17,35 @@
 /***************************************************************************************************
  * BROWSER POLYFILLS
  */
+import 'core-js/es/symbol';
+import 'core-js/es/object';
+import 'core-js/es/function';
+import 'core-js/es/parse-int';
+import 'core-js/es/parse-float';
+import 'core-js/es/number';
+import 'core-js/es/math';
+import 'core-js/es/string';
+import 'core-js/es/date';
+import 'core-js/es/array';
+import 'core-js/es/regexp';
+import 'core-js/es/map';
+import 'core-js/es/weak-map';
+import 'core-js/es/set';
+import 'core-js/es/promise';
+import 'core-js/es/reflect';
+import 'src/assets/rAF.js';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
+import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /**
  * Web Animations `@angular/platform-browser/animations`
  * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
  * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
  */
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+
+
 
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
@@ -52,10 +71,14 @@
  *
  */
 
+(window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
+(window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
+(window as any).__zone_symbol__UNPATCHED_EVENTS = ['scroll', 'mousemove']; // disable patch specified eventNames
+(window as any).__Zone_enable_cross_context_check = true;
+
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'core-js/es/reflect';
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
 
