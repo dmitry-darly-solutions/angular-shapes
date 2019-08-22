61886115fcf24209e425c9af1218f716842d56b1
dmitry-darly-solutions <dmitry@darly.solutions>
2019-08-22 19:52:21 +0300

    Change routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './home/home.component';
+
 const routes: Routes = [
   {
     path: '',
-    loadChildren: './home/home.module#HomeModule',
+    component: HomeComponent
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { InjectionToken, NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { reducers } from './core/store';
+import { HomeComponent } from './home/home.component';
+import { SharedModule } from "./shared/shared.module";
+
+export const APPReducerToken = new InjectionToken(
+  'APP Registered Reducers',
+);
+
+export function getReducers() {
+  return reducers;
+}
+
+export const ReducerProvider = [
+  { provide: APPReducerToken, useFactory: getReducers },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    SharedModule,
+    StoreModule.forRoot(APPReducerToken, {}),
   ],
-  providers: [],
+  providers: [ReducerProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule {
